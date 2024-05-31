f31d64837c68489bda7b6fbeb41ca42e16f398ea
Albert-Andrews <aaaquaye001@st.ug.edu.gh>
2024-05-31 16:27:26 +0000

    environment set up

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.body}>
+        <View style={styles.subContainer}>
+          <View>
+            <Text style={styles.group1Header}>Hello,Devs</Text>
+            <Text style={styles.group1Text}>14 Tasks Today</Text>
+          </View>
+          <TouchableOpacity>
+            <Image source={require("./assets/person.png")} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F7F0E8",
+  },
+  body: {
+   
+    marginHorizontal: 12
+  },
+  subContainer: {
+    padding: 3,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    width: "100%",
+  },
+  group1Header: {
+    fontSize: "32px",
+    lineHeight: "38.4px",
+    fontWeight: "700",
+    width: "170px",
+    height:"37px"
+  },
+  group1Text: {
+    fontSize: "12px",
+    lineHeight:"14.4px",
+    fontWeight:"500"
   },
 });
